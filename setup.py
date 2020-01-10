0ed3daf9fc5b42edf2a470934706dea13a726915
Vivien WALTER <vwalter@johns-imac-2.client.kcl.ac.uk>
2020-01-10 09:40:32 +0000

    Correcting for missing files

--- setup.py
+++ setup.py
@@ -1,18 +1,1 @@
-from setuptools import setup
-
-setup(
-    name="iSCAN",
-    version="0.1",
-    description="Analyse iSCAT pictures",
-    author="Vivien WALTER",
-    author_email="walter.vivien@gmail.com",
-    license="MIT",
-    packages=["iscan"],
-    include_package_data=True,
-    install_requires=[
-        "numpy", "Pillow", "PyQt5", "pims", "scipy", "matplotlib", 'seaborn'
-    ],
-    entry_points = {
-        'console_scripts': ['iscan=iscan:startYU']
-    }
-)
+this is a setup py file useless
